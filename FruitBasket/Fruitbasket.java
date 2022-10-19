d956dcf0d4032e57784acc92dcb9c67014f1bbbc
Atrophlne <92028294+Atrophlne@users.noreply.github.com>
2022-10-19 21:49:16 +0900

    Update Fruitbasket.java

--- FruitBasket/Fruitbasket.java
+++ FruitBasket/Fruitbasket.java
@@ -17,18 +17,18 @@ public class FruitBasket {
       System.out.print("Enter Here >> ");
       int input = scan.nextInt(); // Change input variable
 
-      for (int i = 1; i < input; i++) { // Change input same with Line 18
+      for (int i = 1; i <= input; i++) { // Change input same with Line 18
          System.out.println("Fruits left " + i + "/" + input);
          System.out.print("Enter fruit initial >> "); // Fruit initials are A, O, M, G
          String inp1 = scan.next(); // Change inp1 variable
 
-         if (inp1.equals("a")) { // Change inp1 Same with line 23
+         if (inp1.equals("a") || inp1.equals("A")) { // Change inp1 Same with line 23
             Basket.push(a);
-         } else if (inp1.equals("o")) { // Change inp1 Same with line 23
+         } else if (inp1.equals("o") || inp1.equals("O")) { // Change inp1 Same with line 23
             Basket.push(o);
-         } else if (inp1.equals("m")) { // Change inp1 Same with line 23
+         } else if (inp1.equals("m") || inp1.equals("M")) { // Change inp1 Same with line 23
             Basket.push(m);
-         } else if (inp1.equals("g")) { // Change inp1 Same with line 23
+         } else if (inp1.equals("g") || inp1.equals("G")) { // Change inp1 Same with line 23
             Basket.push(g);
          } else {
             System.out.println("Error"); // Change Error Print
@@ -40,15 +40,15 @@ public class FruitBasket {
 
       while (true) {
          char inp2 = scan.next().charAt(0); // Change inp2 variable
-         if (inp2 == 'e') { // Change inp2 variable same with Line 42
+         if (inp2 == 'e' || inp2 == 'E') { // Change inp2 variable same with Line 42
             Basket.pop();
          }
-         System.out.println("Fruit in the basket" + Basket);
-
          if (Basket.isEmpty()) {
             System.out.println("No more Fruits");
+            break;
          }
-         break;
+         System.out.println("Fruit in the basket" + Basket);
+         System.out.print("Press E to eat a Fruit: ");
       }
    }
 }
